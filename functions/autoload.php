68c5bdd5ea016c91e21d5a6b15546792783289af
Enea <info@overclokk.net>
2021-07-25 17:19:27 +0200

    Moved command for WP-CLI

--- functions/autoload.php
+++ functions/autoload.php
@@ -1,2 +1,13 @@
 <?php
 
+function foo_command( $args ) {
+	\WP_CLI::success( "The script has run!" );
+//	\WP_CLI::success( $args[0] );
+//	\WP_CLI::success( $args[1] );
+
+	$generator = new  \ItalyStrap\ThemeJsonGenerator\Generator(['ciao'=>'bello']);
+	$is_generated = $generator->generate();
+
+	\WP_CLI::success( $is_generated );
+}
+\WP_CLI::add_command( 'foo', 'foo_command' );
