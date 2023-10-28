a1198457e34c3170c1298b4fca121efa8417c4fd
Enea <info@overclokk.net>
2023-10-28 17:56:51 +0200

    Update command alias

--- namespace-bc-aliases.php
+++ namespace-bc-aliases.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 \class_alias(
-	\ItalyStrap\ThemeJsonGenerator\Application\Commands\ComposerPlugin::class,
+	\ItalyStrap\ThemeJsonGenerator\Application\Commands\Command::class,
 	\ItalyStrap\ThemeJsonGenerator\Composer\Plugin::class
 );
 
 \class_alias(
-	\ItalyStrap\ThemeJsonGenerator\Application\Commands\ComposerPlugin::class,
+	\ItalyStrap\ThemeJsonGenerator\Application\Commands\Command::class,
 	ItalyStrap\ThemeJsonGenerator\ComposerPlugin::class
 );
