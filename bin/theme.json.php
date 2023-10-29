52b239e00dd800084d9c1258bf6bb2e836ddd738
Enea <info@overclokk.net>
2023-10-29 20:32:48 +0100

    CS:FIX

--- bin/theme.json.php
+++ bin/theme.json.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require $_composer_autoload_path ?? __DIR__.'/../vendor/autoload.php';
+require $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
 
 use Composer\Console\Application;
 use ItalyStrap\ThemeJsonGenerator\Application\Commands\Composer\ThemeJson;
