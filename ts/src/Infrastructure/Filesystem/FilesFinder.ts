888ac953312271936932d4c0e348296a4e4733e1
Enea <info@overclokk.net>
2024-03-09 19:02:46 +0100

    modify prefix

--- ts/src/Infrastructure/Filesystem/FilesFinder.ts
+++ ts/src/Infrastructure/Filesystem/FilesFinder.ts
@@ -2,7 +2,7 @@ import {File} from '.';
 
 export class FilesFinder {
     public * find(rootFolder: string, extension: string): IterableIterator<File> {
-        const rootFile = `${rootFolder}/theme.json`;
+        const rootFile = `${rootFolder}/theme.${extension}`;
         const rootFileInfo = new File(rootFile);
         if (rootFileInfo.exists()) {
             yield rootFileInfo;
