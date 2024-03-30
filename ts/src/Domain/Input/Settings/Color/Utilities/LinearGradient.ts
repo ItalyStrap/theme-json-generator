c641623027d36e03c4029e8b67284d2dc68de2e0
Enea <info@overclokk.net>
2024-03-30 19:03:47 +0100

    fix bug in LinearGradient

--- ts/src/Domain/Input/Settings/Color/Utilities/LinearGradient.ts
+++ ts/src/Domain/Input/Settings/Color/Utilities/LinearGradient.ts
@@ -2,10 +2,13 @@ import {GradientInterface} from './GradientInterface';
 import {Palette} from '../Palette';
 
 export class LinearGradient implements GradientInterface {
+    private colors: Palette[] = [];
     public constructor(
         private readonly direction: string,
-        private readonly colors: Palette[]
-    ) {}
+        ...colors: Palette[]
+    ) {
+        this.colors = colors;
+    }
 
     public toString(): string {
         return `linear-gradient(${this.direction === '' ? 'to bottom' : this.direction}, ${this.colors.map((color) => color.var()).join(', ')})`;
--- ts/tests/unit/Domain/Input/Settings/Color/Utilities/LinearGradient.test.ts
+++ ts/tests/unit/Domain/Input/Settings/Color/Utilities/LinearGradient.test.ts
@@ -3,23 +3,22 @@ import {LinearGradient} from '../../../../../../../src/Domain/Input/Settings/Col
 import {Palette} from '../../../../../../../src/Domain/Input/Settings/Color';
 import {Color} from '../../../../../../../src/Domain/Input/Settings/Color/Utilities';
 
-const makeInstance = (direction: string, colors: Palette[]) => {
-    return new LinearGradient(direction, colors);
+const makeInstance = (direction: string, ...colors: Palette[]) => {
+    return new LinearGradient(direction, ...colors);
 };
 
 describe('LinearGradient class', () => {
     test('LinearGradient', () => {
-        const sut = makeInstance('to right', [
-            new Palette('base', 'Brand base color', new Color('hsla(212,73%,55%,1)')),
-        ]);
+        const sut = makeInstance('to right', new Palette('base', 'Brand base color', new Color('hsla(212,73%,55%,1)')));
         expect(sut).toBeInstanceOf(LinearGradient);
     });
 
     test('LinearGradient render', () => {
-        const sut = makeInstance('to right', [
+        const sut = makeInstance(
+            'to right',
             new Palette('base', 'Brand base color', new Color('hsla(212,73%,55%,1)')),
-            new Palette('light', 'Lighter color', new Color('hsla(0,0%,100%,1)')),
-        ]);
+            new Palette('light', 'Lighter color', new Color('hsla(0,0%,100%,1)'))
+        );
 
         expect(sut.toString()).toBe(
             'linear-gradient(to right, var(--wp--preset--color--base), var(--wp--preset--color--light))'
